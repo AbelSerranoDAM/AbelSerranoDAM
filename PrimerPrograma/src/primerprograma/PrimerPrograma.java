e6d0689b6ae1cf52b15877f37bf9465897fd7d48
SergiGonzalezDAM <job_sergi@hotmail.es>
2016-09-15 19:27:31 +0200

    Update PrimerPrograma.java

--- PrimerPrograma/src/primerprograma/PrimerPrograma.java
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -3,7 +3,7 @@ package primerprograma;
 public class PrimerPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo, que mal esta todo esto");
     }
 
 }
